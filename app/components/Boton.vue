d66b06f6fd0fa4093ed659dc276fd4e86c0f4367
Sandra Gómez Poveda <sandragomezpoveda@MacBook-Pro-de-Sandra.local>
2026-05-15 17:22:45 +0200

    boton primario con hover

---
+++ app/components/Boton.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  texto: String
+})
+</script>
+
+<template>
+  <button class="button-primary">
+    {{ texto }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button-primary {
+  font-family: 'Zilla Slab', serif;
+  font-size: 32px;
+
+  background-color: #BFFF00;
+  color: #C8D4A6;
+
+  border: none;
+  border-radius: 55px;
+
+  padding: 16px 32px;
+  cursor: pointer;
+
+  transition: all 0.3s ease;
+
+  &:hover {
+    background-color: #C8D4A6;
+    color: #BFFF00;
+  }
+}
+</style>
